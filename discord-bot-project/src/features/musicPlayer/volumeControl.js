01946ae923d42b5fac1b00f0cbd22f33d4fded30
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-08 16:39:45 +0530

    Generated: `discord-bot-project/src/features/musicPlayer/volumeControl.js`

---
+++ discord-bot-project/src/features/musicPlayer/volumeControl.js
@@ -0,0 +1,19 @@
+const volumeControl = {
+  setVolume: (volume) => {
+    // Logic to set the volume of the music player
+  },
+  increaseVolume: () => {
+    // Logic to increase the volume of the music player
+  },
+  decreaseVolume: () => {
+    // Logic to decrease the volume of the music player
+  },
+  muteVolume: () => {
+    // Logic to mute the volume of the music player
+  },
+  unmuteVolume: () => {
+    // Logic to unmute the volume of the music player
+  }
+};
+
+module.exports = volumeControl;
