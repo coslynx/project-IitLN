382ae09a6d0b8c45e73b9a4de2cf3c43260a3960
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-08 16:39:15 +0530

    Generated: `discord-bot-project/src/commands/gameCommands.js`

---
+++ discord-bot-project/src/commands/gameCommands.js
@@ -0,0 +1,19 @@
+const gameCommands = {
+  playTriviaGame: () => {
+    // Logic for playing trivia game
+  },
+  playWordGame: () => {
+    // Logic for playing word game
+  },
+  playPuzzleGame: () => {
+    // Logic for playing puzzle game
+  },
+  displayUserScores: () => {
+    // Logic for displaying user scores
+  },
+  showLeaderboard: () => {
+    // Logic for showing leaderboard
+  },
+};
+
+module.exports = gameCommands;
