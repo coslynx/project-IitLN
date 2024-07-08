edb504f0e968363958f6edb67998326762e785af
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-08 16:39:42 +0530

    Generated: `discord-bot-project/src/features/musicPlayer/skip.js`

---
+++ discord-bot-project/src/features/musicPlayer/skip.js
@@ -0,0 +1,19 @@
+const { queue } = require('./play');
+
+module.exports = {
+  name: 'skip',
+  description: 'Skip the currently playing song',
+
+  execute(message) {
+    const serverQueue = queue.get(message.guild.id);
+
+    if (!message.member.voice.channel) {
+      return message.channel.send('You need to be in a voice channel to skip the music!');
+    }
+    if (!serverQueue) {
+      return message.channel.send('There is no song that I could skip!');
+    }
+
+    serverQueue.connection.dispatcher.end();
+  },
+};
